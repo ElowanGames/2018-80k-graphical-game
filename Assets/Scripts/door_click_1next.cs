ba8977edab4f6945eec8bfd7ae928689d9aa90fd
Elowan <elowangames@gmail.com>
2018-07-19 16:38:05 -0700

    Added scene management to doors (so clicking on a door means you go to the appropriate scene)

---
+++ Assets/Scripts/door_click_1next.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class door_click_1next : MonoBehaviour {
+
+	void OnMouseDown()
+    {
+        print("Clicked");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
