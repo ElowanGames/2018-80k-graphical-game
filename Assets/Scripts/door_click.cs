430f69bc4335f01da638581f060bde045a5e6d9a
Elowan <elowangames@gmail.com>
2018-07-19 15:33:31 -0700

    Added Door prefab with clicking interaction

---
+++ Assets/Scripts/door_click.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door_click : MonoBehaviour {
+
+	void OnMouseDown()
+    {
+        print("Clicked");
+        // Change this later to transition to the appropriate scene
+    }
+}
