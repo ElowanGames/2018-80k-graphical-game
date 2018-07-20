61f2eee0cd31c55e452be169b14df6ab7d725ec0
Elowan <elowangames@gmail.com>
2018-07-20 00:29:57 -0700

    Minigame 1: Added clickable clouds that change to sun (correct) and rain (incorrect) respectively when clicked by player

---
+++ Assets/Scripts/cloud_click_correct.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cloud_click_correct : MonoBehaviour
+{
+    public Sprite sun;
+    SpriteRenderer sr;
+
+    // Use this for initialization
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Change sprites on click
+    void OnMouseDown()
+    {
+        sr.sprite = sun;
+    }
+}
---
+++ Assets/Scripts/cloud_click_incorrect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cloud_click_incorrect : MonoBehaviour
+{
+    public Sprite rain;
+    SpriteRenderer sr;
+
+    // Use this for initialization
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Change sprites on click
+    void OnMouseDown()
+    {
+        sr.sprite = rain;
+    }
+}
