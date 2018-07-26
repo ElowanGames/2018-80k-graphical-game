18c7628f98775a5581f8f2871aa5be01a80f658b
Elowan <elowangames@gmail.com>
2018-07-26 01:06:44 -0700

    Added Minigame2 message clicking scripts

---
+++ Assets/current_level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class current_level : MonoBehaviour
+{
+    public int the_level = 1;
+
+    // Use this for initialization
+    private void UpdateLevel()
+    {
+        the_level++;
+    }
+}
---
+++ Assets/level_checker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level_checker : MonoBehaviour
+{
+    public int level;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var curr = GameObject.Find("name here").GetComponent<current_level>();
+        if (level < curr.the_level)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
