cbe5e98d2f3153d74e53ddc9c31cb7d951171838
veenna <veennasf@gmail.com>
2018-07-25 21:18:14 -0700

    Reordered text scenes

---
+++ Assets/TextContinue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextContinue : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        print("Clicked");
+        SceneManager.LoadScene("Scene02_B");
+    }
+}
---
+++ Assets/textContinue2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class textContinue2 : MonoBehaviour {
+    void OnMouseDown()
+    {
+        print("Clicked");
+        SceneManager.LoadScene("Scene02_Doors1");
+    }
+}
